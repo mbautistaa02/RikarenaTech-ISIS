f6a255bc0cd7a0104a2c9f71581df2da9f35c4a9
Andres-cmk <ramirezmontanaandresfelipe@gmail.com>
2025-12-06 18:19:56 -0500

    update urls from Auth app

--- Project/backend/AuthenticationProject/urls.py
+++ Project/backend/AuthenticationProject/urls.py
@@ -3,6 +3,8 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
+     # OAuth login/logout (allauth)
     path("auth/", include("allauth.urls")),
     path("token/", views.get_jwt_token, name="get_token"),
+    path("", views.home, name="home"),
 ]
