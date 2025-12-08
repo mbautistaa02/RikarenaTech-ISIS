5673d99e34c8840659de38bbba4aa17ae05c452e
Andres-cmk <ramirezmontanaandresfelipe@gmail.com>
2025-12-08 18:11:26 -0500

    remove descriptions in User

--- Project/backend/users/serializers.py
+++ Project/backend/users/serializers.py
@@ -51,5 +51,4 @@ class SellerUserSerializer(serializers.ModelSerializer):
             "latest_post_date",
             "municipality_name",
             "department_name",
-            "description",  # New Field for sellers
         ]
